1cea1f0de61e881c2d1c640b389eb186bda2e0c5
Anthony Gallegos <59071108+galleg46@users.noreply.github.com>
2021-10-26 17:17:20 -0500

    File upload
    Uploaded files from Desktop

---
+++ src/edu/uwm/cs351/ManyParticles.java
@@ -0,0 +1,56 @@
+//Anthony Gallegos
+package edu.uwm.cs351;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ManyParticles extends JPanel {
+
+	/**
+	 * Put this in to keep Eclipse happy. 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final ParticleSeq all;
+	
+	public ManyParticles(ParticleSeq ps) {
+		all = ps;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		final ParticleSeq copy = all.clone();
+		// NB: This might run the same time as move()
+		// and so we need to use a copy.
+		// HINT: you can use p.draw(g) for a particle p
+		for(copy.start(); copy.isCurrent(); copy.advance())
+		{
+			copy.getCurrent().draw(g);
+		}
+	}
+	
+	public void move() {
+		ParticleSeq copy = all.clone();
+		for (all.start(); all.isCurrent(); all.advance()) {
+			Vector force = new Vector();
+			Particle p = all.getCurrent();
+			// System.out.println("Force on " + p);
+			for (copy.start(); copy.isCurrent(); copy.advance()) {
+				Particle q = copy.getCurrent();
+				// System.out.println("  from " + q);
+				if (p != q) {
+					force = force.add(q.gravForceOn(p));
+				}
+			}
+			p.applyForce(force);
+		}
+		// HINT: You can use p.move() for a particle p
+		for(all.start(); all.isCurrent(); all.advance())
+		{
+			all.getCurrent().move();
+		}
+		repaint();
+	}
+}
